66389b02b337710f642475c45a6b52293391f19f
fateking27 <fateking27@outlook.com>
2023-09-28 15:11:16 +0800

    用户添加

--- src/views/system/user/UserAdd.vue
+++ src/views/system/user/UserAdd.vue
@@ -2,16 +2,30 @@
     <a-button type="primary" @click="showDrawer">新增用户</a-button>
     <a-drawer v-model:open="open" title="新增用户" placement="right" width="500px">
 
-        <a-form :model="formState" v-bind="layout" name="nest-messages"
-            @finish="onFinish">
+        <a-form :model="formState" v-bind="layout" name="nest-messages" @finish="onFinish">
             <a-form-item :name="['user', 'name']" label="用户名" :rules="[{ required: true }]">
                 <a-input v-model:value="formState.user.name" />
             </a-form-item>
-            <a-form-item :name="['user', 'email']" label="密码">
+            <a-form-item :name="['user', 'password']" label="密码" :rules="[{ required: true }]">
+                <a-input v-model:value="formState.user.password" />
+            </a-form-item>
+            <a-form-item :name="['user', 'email']" label="邮箱">
                 <a-input v-model:value="formState.user.email" />
             </a-form-item>
-            <a-form-item :name="['user', 'website']" label="邮箱">
-                <a-input v-model:value="formState.user.website" />
+            <a-form-item :name="['user', 'phone']" label="手机">
+                <a-input v-model:value="formState.user.phone" />
+            </a-form-item>
+            <a-form-item :name="['user', 'state']" label="状态" :rules="[{ required: true }]">
+                <a-radio-group v-model:value="formState.user.state">
+                    <a-radio value="0">锁定</a-radio>
+                    <a-radio value="1">有效</a-radio>
+                </a-radio-group>
+            </a-form-item>
+            <a-form-item :name="['user', 'sexx']" label="性别" :rules="[{ required: true }]">
+                <a-radio-group v-model:value="formState.user.sexx">
+                    <a-radio value="1">男</a-radio>
+                    <a-radio value="0">女</a-radio>
+                </a-radio-group>
             </a-form-item>
             <a-form-item>
                 <a-button type="primary" html-type="submit">提交</a-button>
@@ -37,10 +51,11 @@ const layout = {
 const formState = reactive({
     user: {
         name: '',
-        age: '',
+        password: '',
+        phone: '',
         email: '',
-        website: '',
-        introduction: '',
+        state: '',
+        sexx: ''
     },
 });
 const onFinish = (values: any) => {
